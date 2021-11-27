3fe4333c428d4cc1f3c791024164e31364dc0ec3
Alexandre <alexandreeduardolemes@gmail.com>
2021-11-27 02:13:47 -0300

    exercicios seguintes

--- fundamentos/bloco4/dia1/variaveis.js
+++ fundamentos/bloco4/dia1/variaveis.js
@@ -1,6 +1,9 @@
-const base = 5;
-let height = 8;
+const nota = 100;
 
-const area = base * height;
-console.log(area);
-
+if (nota >=80) {
+console.log("Parabéns, você foi aprovado!")
+} else if (nota < 80 && nota >= 60) {
+    console.log("Voce esta na nossa lista de espera!")
+} else {
+    console.log("Voce foi reprovado")
+} 
