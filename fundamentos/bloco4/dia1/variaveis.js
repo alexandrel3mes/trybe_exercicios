6a0635fc03c03e9e6139d1cf28217fde45c6af4e
Alexandre <alexandreeduardolemes@gmail.com>
2021-11-27 21:48:08 -0300

    todos feitos :)

--- fundamentos/bloco4/dia1/variaveis.js
+++ fundamentos/bloco4/dia1/variaveis.js
@@ -91,3 +91,188 @@ if (nota >= 90) {
 } else if (nota > 100 || nota < 0) {
     console.log("error");
 } 
+
+
+//// Exercicio 8
+
+const num1;
+const num2;
+const num3;
+
+if (num1 % 2 == 0 || num2 % 2 == 0 || num3 % 2 == 0) {
+    console.log(true)
+} else {
+    console.log(false)
+}
+
+//// Exercicio 9
+
+const num4;
+const num5;
+const num6;
+
+if (num4 % 2 != 0 || num5 % 2 != 0 || num6 % 2 != 0) {
+    console.log(true)
+} else {
+    console.log(false)
+}
+
+//// Exercicio 10
+
+const custo = null;
+const imposto = custo *0.2;
+const custoTotal = custo + imposto;
+const valorDeVenda = null;
+
+let lucro = valorDeVenda - custoTotal;
+
+console.log(lucro * 1000);
+
+//// Exercicio 11
+
+const salarioBruto = null;
+let salarioBase = undefined;
+let ir = undefined;
+
+
+
+if (salarioBruto <= 1556.94) {
+
+    let salarioBase = salarioBruto - (salarioBruto * 0.08);
+    if (salarioBase <= 1903.98) {
+
+        let ir = 0;
+        console.log(salarioBase - ir);
+    
+    
+    } else if (salarioBase >= 1903.99 || salarioBase <= 2826.65) {
+    
+        let ir = (salarioBase * 0.075) - 142.80;
+        console.log(salarioBase - ir);
+     
+    
+    } else if (salarioBase >= 2826.66 || salarioBase <= 3751.05) {
+    
+        let ir = (salarioBase * 0.15) - 354.80;
+        console.log(salarioBase - ir);
+        
+    } else if (salarioBase >= 3751.06 || salarioBase <= 4664.68) {
+    
+        let ir = (salarioBase * 0.22) - 636.13;
+        console.log(salarioBase - ir);
+      
+    
+    } else if (salarioBase > 4664.68) {
+    
+        let ir = (salarioBase * 0.275) - 869.36;
+        console.log(salarioBase - ir);
+    
+    }
+    
+
+
+} else if (salarioBruto >= 1556.95 || salarioBruto <= 2594.92) {
+
+    let salarioBase = salarioBruto - (salarioBruto * 0.09);
+    if (salarioBase <= 1903.98) {
+
+        let ir = 0;
+        console.log(salarioBase - ir);
+    
+    
+    } else if (salarioBase >= 1903.99 || salarioBase <= 2826.65) {
+    
+        let ir = (salarioBase * 0.075) - 142.80;
+        console.log(salarioBase - ir);
+     
+    
+    } else if (salarioBase >= 2826.66 || salarioBase <= 3751.05) {
+    
+        let ir = (salarioBase * 0.15) - 354.80;
+        console.log(salarioBase - ir);
+        
+    } else if (salarioBase >= 3751.06 || salarioBase <= 4664.68) {
+    
+        let ir = (salarioBase * 0.22) - 636.13;
+        console.log(salarioBase - ir);
+      
+    
+    } else if (salarioBase > 4664.68) {
+    
+        let ir = (salarioBase * 0.275) - 869.36;
+        console.log(salarioBase - ir);
+    
+    }
+
+
+
+} else if (salarioBruto >= 2594.93 || salarioBruto <= 5189.82) {
+
+    let salarioBase = salarioBruto - (salarioBruto * 0.11);
+    if (salarioBase <= 1903.98) {
+
+        let ir = 0;
+        console.log(salarioBase - ir);
+    
+    
+    } else if (salarioBase >= 1903.99 || salarioBase <= 2826.65) {
+    
+        let ir = (salarioBase * 0.075) - 142.80;
+        console.log(salarioBase - ir);
+     
+    
+    } else if (salarioBase >= 2826.66 || salarioBase <= 3751.05) {
+    
+        let ir = (salarioBase * 0.15) - 354.80;
+        console.log(salarioBase - ir);
+        
+    } else if (salarioBase >= 3751.06 || salarioBase <= 4664.68) {
+    
+        let ir = (salarioBase * 0.22) - 636.13;
+        console.log(salarioBase - ir);
+      
+    
+    } else if (salarioBase > 4664.68) {
+    
+        let ir = (salarioBase * 0.275) - 869.36;
+        console.log(salarioBase - ir);
+    
+    }
+
+
+} else if (salarioBruto > 5189.82) {
+
+    let salarioBase = salarioBruto - 570.88;
+    if (salarioBase <= 1903.98) {
+
+        let ir = 0;
+        console.log(salarioBase - ir);
+    
+    
+    } else if (salarioBase >= 1903.99 || salarioBase <= 2826.65) {
+    
+        let ir = (salarioBase * 0.075) - 142.80;
+        console.log(salarioBase - ir);
+     
+    
+    } else if (salarioBase >= 2826.66 || salarioBase <= 3751.05) {
+    
+        let ir = (salarioBase * 0.15) - 354.80;
+        console.log(salarioBase - ir);
+        
+    } else if (salarioBase >= 3751.06 || salarioBase <= 4664.68) {
+    
+        let ir = (salarioBase * 0.22) - 636.13;
+        console.log(salarioBase - ir);
+      
+    
+    } else if (salarioBase > 4664.68) {
+    
+        let ir = (salarioBase * 0.275) - 869.36;
+        console.log(salarioBase - ir);
+    
+    }
+  
+
+}
+
