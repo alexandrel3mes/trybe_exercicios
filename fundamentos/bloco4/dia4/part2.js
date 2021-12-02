513cbbccce19ffd005b2ca32a2f8bd21f6a61353
Alexandre <alexandreeduardolemes@gmail.com>
2021-12-02 02:58:26 -0300

    parte 2 feita

--- fundamentos/bloco4/dia4/part2.js
+++ fundamentos/bloco4/dia4/part2.js
@@ -156,3 +156,23 @@ function somatorio (n) {
 }
 
 console.log(somatorio(5));
+
+/* 7 - Crie uma função que receba uma string word e outra string ending . Verifique se a string ending é o final da string word . Considere que a string ending sempre será menor que a string word .
+Valor de teste: 'trybe' e 'be'
+Valor esperado no retorno da função: true
+verificaFimPalavra('trybe', 'be') ;
+Retorno esperado: true
+verificaFimPalavra('joaofernando', 'fernan') ;
+Retorno esperado: false
+ */
+
+
+function verificaFimPalavra (stringWord, stringEnd) {
+
+
+    return stringWord.substring(stringWord.length - stringEnd.length) === stringEnd;
+
+}
+
+console.log(verificaFimPalavra("trybe", "be"));
+console.log(verificaFimPalavra('joaofernando', 'fernan'));
