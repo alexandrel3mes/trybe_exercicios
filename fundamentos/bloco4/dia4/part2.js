18446b1e1b9071bc3ec862b518f4ed2a642c05d0
Alexandre <alexandreeduardolemes@gmail.com>
2021-12-02 00:54:03 -0300

    2 e 3 feitos

--- fundamentos/bloco4/dia4/part2.js
+++ fundamentos/bloco4/dia4/part2.js
@@ -25,3 +25,57 @@ function verificaPalindrome (palavra) {
 }
 
 console.log(verificaPalindrome("arara"));
+
+
+/* 2 - Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
+Array de teste: [2, 3, 6, 7, 10, 1]; .
+Valor esperado no retorno da função: 4 .
+ */
+
+function indiceDoMaiorValor (arrayTeste) {
+
+    let maiorIndice = 0;
+
+    for(let value in arrayTeste) {
+
+        if (arrayTeste[maiorIndice] < arrayTeste[value]) {
+            maiorIndice = value
+        }
+
+    }
+
+    return maiorIndice;
+
+}
+
+console.log(indiceDoMaiorValor([2, 3, 6, 7, 10, 1]));
+
+
+/* 3 - Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
+Array de teste: [2, 4, 6, 7, 10, 0, -3]; .
+Valor esperado no retorno da função: 6 . */
+
+function indiceDoMenorValor (arrayTeste) {
+
+    let menorIndice = 0;
+
+    for(let value in arrayTeste) {
+
+        if (arrayTeste[menorIndice] > arrayTeste[value]) {
+            menorIndice = value
+        }
+
+    }
+
+    return menorIndice;
+
+}
+
+console.log(indiceDoMenorValor([2, 4, 6, 7, 10, 0, -3]));
+
+
+
+/* 4 - Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
+Array de teste: ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']; .
+Valor esperado no retorno da função: Fernanda . */
+
