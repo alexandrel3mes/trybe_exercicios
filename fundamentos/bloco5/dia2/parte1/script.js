66bd3b3b8cff87db73a628df291909e5dbbc8258
Alexandre <alexandreeduardolemes@gmail.com>
2021-12-06 18:55:12 -0300

    parte 1 e 2 feitas

---
+++ fundamentos/bloco5/dia2/parte1/script.js
@@ -0,0 +1,68 @@
+/* 
+Acesse o elemento elementoOndeVoceEsta .
+Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele. */
+
+document.getElementById("elementoOndeVoceEsta").parentElement.style.color = "red";
+
+
+/* Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso? */
+
+document.getElementById("elementoOndeVoceEsta").firstElementChild.innerText = "Texto do primeiro elemento filho";
+
+
+/* Acesse o primeiroFilho a partir de pai . */
+
+document.getElementById("pai").firstElementChild;
+
+/* Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta . */
+
+document.getElementById("elementoOndeVoceEsta").previousElementSibling;
+
+/* Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta . */
+
+document.getElementById("elementoOndeVoceEsta").nextSibling;
+
+/* Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta . */
+
+document.getElementById("elementoOndeVoceEsta").nextElementSibling;
+
+/* Agora acesse o terceiroFilho a partir de pai . */
+
+document.getElementById("pai").lastElementChild.previousElementSibling;
+
+
+
+// Parte 2
+
+/* Crie um irmão para elementoOndeVoceEsta . */
+
+let irmaoDoElemento = document.createElement("section");
+
+document.getElementById("pai").appendChild(irmaoDoElemento);
+
+irmaoDoElemento.setAttribute('id', 'irmaoDoElemento')
+
+
+/* Crie um filho para elementoOndeVoceEsta  */
+
+let filhoDoOnde = document.createElement("section");
+
+document.getElementById("elementoOndeVoceEsta").appendChild(filhoDoOnde);
+
+filhoDoOnde.setAttribute('id', 'filhoDoOnde');
+
+/* Crie um filho para primeiroFilhoDoFilho . */
+
+let netoDeOnde = document.createElement("section");
+
+document.getElementById("primeiroFilhoDoFilho").appendChild(netoDeOnde);
+
+netoDeOnde.setAttribute('id', 'netoDoOnde');
+
+/* A partir desse filho criado, acesse terceiroFilho . */
+
+document.getElementById("netoDoOnde").parentElement.parentElement.nextElementSibling;
+
+
+
+// Parte 3
