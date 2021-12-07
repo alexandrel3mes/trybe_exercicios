67efdc84ff3d26b9a3e295ede5ee5de554710072
Alexandre <alexandreeduardolemes@gmail.com>
2021-12-07 09:22:30 -0300

    1 feito

--- fundamentos/bloco5/dia3/calendario_tryber/script.js
+++ fundamentos/bloco5/dia3/calendario_tryber/script.js
@@ -14,3 +14,27 @@ function createDaysOfTheWeek() {
   createDaysOfTheWeek();
   
   // Escreva seu código abaixo.
+
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];;
+
+      let lista = document.querySelector('#days');
+
+      for (index of dezDaysList) {
+          let itemDaLista = index;
+
+          let listaItem = document.createElement('li');
+          listaItem.setAttribute('class', 'day');
+
+          if (index == 24 || index == 25 || index == 31){
+            listaItem.setAttribute('class', 'day holiday');
+          }
+
+          if (index == 4 || index == 11 || index == 18 || index == 25){
+            listaItem.setAttribute('class', 'day friday');
+          }
+
+
+          listaItem.innerText = itemDaLista;
+
+          lista.appendChild(listaItem);
+      }
