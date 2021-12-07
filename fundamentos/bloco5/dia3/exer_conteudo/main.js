acf7e167a16ba5a46ca7b538f475acd88bc30d22
Alexandre <alexandreeduardolemes@gmail.com>
2021-12-07 08:52:47 -0300

    parte de conteudo feita

--- fundamentos/bloco5/dia3/exer_conteudo/main.js
+++ fundamentos/bloco5/dia3/exer_conteudo/main.js
@@ -10,17 +10,73 @@ const myWebpage = document.getElementById('my-spotrybefy');
 // 1.2. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
 
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
+
+
+firstLi.addEventListener("click", liUmClicado);
+secondLi.addEventListener("click", liDoisClicado);
+thirdLi.addEventListener("click", liTresClicado);
+
+function liUmClicado () {
+  firstLi.setAttribute('class', 'tech');
+  secondLi.setAttribute('class', 'non_tech');
+  thirdLi.setAttribute('class', 'non_tech');
+}
+
+function liDoisClicado () {
+    secondLi.setAttribute('class', 'tech');
+    firstLi.setAttribute('class', 'non_tech');
+    thirdLi.setAttribute('class', 'non_tech');
+}
+
+function liTresClicado () {
+    thirdLi.setAttribute('class', 'tech');
+    firstLi.setAttribute('class', 'non_tech');
+    secondLi.setAttribute('class', 'non_tech');
+}
+
+
+
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+
+
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
+
+input.addEventListener("keydown", escreveTech) 
+
+function escreveTech () {
+  let inputValue = input.value;
+  document.querySelector(".tech").innerText = inputValue;
+}
+
 
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portifólio?
 
+myWebpage.addEventListener("dblclick", duploClique)
+
+function duploClique () {
+  window.open("https://alexandrel3mes.github.io/", "_blank")
+}
+
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
+
+
+myWebpage.addEventListener("mouseenter", passaMouse);
+
+const randColor = () =>  {
+  return "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase();
+}
+
+
+
+function passaMouse () {
+  myWebpage.style.color = randColor();
+}
+
 
 // Segue abaixo um exemplo do uso de event.target:
 
