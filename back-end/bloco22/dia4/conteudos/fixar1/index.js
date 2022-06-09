951d32e842a21dbcc456cc82b7516498491f7548
Alexandre <alexandreeduardolemes@gmail.com>
2022-06-09 18:52:03 -0300

    Fixação até query string feito

---
+++ back-end/bloco22/dia4/conteudos/fixar1/index.js
@@ -0,0 +1,74 @@
+/* index.js */
+const express = require('express');
+const app = express();
+
+const sortObj = require('./compareValues');
+
+const recipes = [
+  { id: 1, name: 'Lasanha', price: 40.0, waitTime: 30 },
+  { id: 2, name: 'Macarrão a Bolonhesa', price: 35.0, waitTime: 25 },
+  { id: 3, name: 'Macarrão com molho branco', price: 35.0, waitTime: 25 },
+];
+
+const drinks = [
+	{ id: 1, name: 'Refrigerante Lata', price: 5.0 },
+	{ id: 2, name: 'Refrigerante 600ml', price: 8.0 },
+	{ id: 3, name: 'Suco 300ml', price: 4.0 },
+	{ id: 4, name: 'Suco 1l', price: 10.0 },
+	{ id: 5, name: 'Cerveja Lata', price: 4.5 },
+	{ id: 6, name: 'Água Mineral 500 ml', price: 5.0 },
+];
+
+app.get('/recipes', function (req, res) {
+  res.json(recipes.sort(sortObj('name')));
+});
+
+app.get('/recipes/search', function (req, res) {
+  const { name, maxPrice, minPrice } = req.query;
+  if (minPrice && maxPrice) {
+    const filteredRecipes = recipes.filter((r) => r.name.toLowerCase().includes(name.toLowerCase()) && r.price <= Number(maxPrice) && r.price >= Number(minPrice));
+    res.status(200).json(filteredRecipes);
+  } else if(!minPrice) {
+    const filteredRecipes = recipes.filter((r) => r.name.toLowerCase().includes(name.toLowerCase()) && r.price <= Number(maxPrice));
+    res.status(200).json(filteredRecipes);
+  } else if (!maxPrice) {
+    const filteredRecipes = recipes.filter((r) => r.name.toLowerCase().includes(name.toLowerCase()) && r.price >= Number(minPrice));
+    res.status(200).json(filteredRecipes);
+  }
+});
+
+
+app.get('/recipes/:id', function (req, res) {
+  const { id } = req.params;
+  const recipe = recipes.find((r) => r.id === Number(id));
+
+  if (!recipe) return res.status(404).json({ message: 'Recipe not found!'});
+
+  res.status(200).json(recipe);
+});
+
+
+
+app.get('/drinks', function (req, res) {
+  res.json(drinks.sort(sortObj('name')));
+});
+
+app.get('/drinks/search', function (req, res) {
+  const { name } = req.query;
+  const filteredDrinks = drinks.filter((d) => d.name.toLowerCase().includes(name.toLowerCase()));
+  res.status(200).json(filteredDrinks);
+});
+
+app.get('/drinks/:id', function (req, res) {
+  const { id } = req.params;
+  const drink = drinks.find((r) => r.id === Number(id));
+
+  if (!drink) return res.status(404).json({ message: 'Drink not found!'});
+
+  res.status(200).json(drink);
+});
+
+
+app.listen(3001, () => {
+  console.log('Aplicação ouvindo na porta 3001');
+});
