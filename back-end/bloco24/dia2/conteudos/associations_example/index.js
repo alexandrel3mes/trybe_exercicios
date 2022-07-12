1333f28462d9e7a536927d37af0557549e9faaa6
Alexandre <alexandreeduardolemes@gmail.com>
2022-07-12 00:39:15 -0300

    Conteudo feito ate eager vs lazy loading

---
+++ back-end/bloco24/dia2/conteudos/associations_example/index.js
@@ -0,0 +1,56 @@
+// index.js
+const express = require('express');
+const { Address, Employee } = require('./models');
+
+const app = express();
+
+app.get('/employees', async (_req, res) => {
+  try {
+    const employees = await Employee.findAll({
+      include: { model: Address, as: 'addresses' },
+    });
+
+    return res.status(200).json(employees);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Ocorreu um erro' });
+  };
+});
+
+app.get('/adresses', async (_req, res) => {
+  try {
+    const adresses = await Address.findAll({
+      include: { model: Employee, as: 'employees' },
+    });
+
+    return res.status(200).json(adresses);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Ocorreu um erro' });
+  };
+});
+
+app.get('/employees/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const employee = await Employee.findOne({ where: { id } });
+
+    if (!employee) res.status(404).json({ message: 'Funcionário não encontrado' });
+
+    if (req.query.includeAddresses === 'true') {
+      const addresses = await Address.findAll({ where: { employeeId: id } });
+      return res.status(200).json({ employee, addresses });
+    }
+    
+    return res.status(200).json(employee);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  };
+});
+
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}`));
+
+module.exports = app;
