88990b4f03183f5603ab044cfbf26e64da21c9f8
Alexandre <alexandreeduardolemes@gmail.com>
2022-08-08 18:42:20 -0300

    mais exercicios

---
+++ back-end/bloco26/dia1/exercicios/my-ts-scripts/enums.ts
@@ -0,0 +1,21 @@
+enum Months {
+  January = 1,
+  February,
+  March,
+  April,
+  May,
+  June,
+  July,
+  August,
+  September,
+  October,
+  November,
+  December,
+}
+
+enum Seasons {
+  Spring = 1,
+  Summer,
+  Fall,
+  Winter,
+}
